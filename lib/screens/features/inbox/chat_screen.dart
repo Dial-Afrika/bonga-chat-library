5520cae4cd1b68dab7418f9c5ce04cd6358348c1
thai-ru <robinthairu34@gmail.com>
2023-08-08 16:03:20 +0300

    SendMessage to Bonga

--- lib/screens/features/inbox/chat_screen.dart
+++ lib/screens/features/inbox/chat_screen.dart
@@ -1,104 +1,193 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:socket_io_client/socket_io_client.dart' as io;
+import 'package:socket_io_client/socket_io_client.dart' as IO;
+import 'package:http/http.dart' as http;
 
 class ChatScreen extends StatefulWidget {
-  const ChatScreen({super.key});
+  final String clientId;
+  final String ticketId;
+  final String socketId;
+  final IO.Socket socket;
+  const ChatScreen(
+      {
+        required this.clientId,
+        required this.ticketId,
+        required this.socketId,
+        required this.socket,
+      super.key,
+      });
 
   @override
   _ChatScreenState createState() => _ChatScreenState();
 }
 
 class _ChatScreenState extends State<ChatScreen> {
-  final TextEditingController _messageController = TextEditingController();
-  final List<ChatMessage> _messages = [];
-  late io.Socket socket;
-  late String clientId;
-  late String ticketId;
-  bool chatInitialized = false;
+  late IO.Socket socket;
+  TextEditingController _messageController = TextEditingController();
+  List<Widget> _messages = [];
 
   @override
   void initState() {
     super.initState();
-    initializeSocket();
-  }
+    socket = widget.socket; // Use the provided socket connection
 
-  void initializeSocket() {
-    socket = io.io('https://chatdesk-prod.dialafrika.com/webchat/1/process', <String, dynamic>{
-      'transports': ['websocket'],
-    });
+    // Subscribe to 'message_from_agent' event
+    socket.on('message_from_agent', (data) {
+      // Handle incoming messages from the agent
+      print('Received message from agent in ChatScreen: $data');
 
-    socket.on('connect', (_) {
-      print('Socket connected');
-
-      // Initiate the chat and handle the response
-      socket.emit('initChat', {"route": "LIVE_CHAT"});
-      socket.on('initChatResponse', (data) {
-        setState(() {
-          clientId = data['data']['payload']['message']['clientId'];
-          ticketId = data['data']['payload']['message']['ticketId'];
-          // Here, you can handle the welcome message and the sample payload
-          String welcomeMessage = data['data']['message'];
-          var payloadData = data['data']['payload']['data'];
-          _messages.add(ChatMessage(
-            senderId: clientId,
-            clientId: clientId,
-            text: welcomeMessage,
-          ));
-          chatInitialized = true;
-        });
+      setState(() {
+        final message = data['message'];
+        _messages.add(ChatMessage(
+          senderId: message['clientId'],
+          text: message['message'],
+          clientId: widget.clientId,
+          timestamp: DateTime.parse(message['created_at']),
+        ));
       });
     });
 
+    // Subscribe to 'message' event
     socket.on('message', (data) {
-      print('Received message: $data');
+      final message = data['message'];
+
       setState(() {
         _messages.add(ChatMessage(
-          senderId: data['senderId'],
-          text: data['text'],
-          clientId: 'clientId',
-
+          senderId: message['clientId'],
+          text: message['message'],
+          clientId: widget.clientId,
+          timestamp: DateTime.parse(message['created_at']),
         ));
       });
     });
 
-    socket.connect();
+    print(
+        "ChatScreen initialized with clientId: ${widget.clientId} and ticketId: ${widget.ticketId}");
   }
 
 
-  void sendMessage() {
-    if (_messageController.text.trim().isNotEmpty && chatInitialized) {
-      final message = {
-        'senderId': clientId,
-        'text': _messageController.text.trim(),
-      };
-      socket.emit('message', message);
-      setState(() {
-        _messages.add(ChatMessage(
-          senderId: clientId,
-          text: _messageController.text.trim(),
-          clientId: clientId,
+  void sendMessage(String message) async {
+    final messageData = {
+      "route": "LIVE_CHAT",
+      "payload": {
+        "clientMessage": message,
+        "socketId": widget.socketId,
+        "ticketId": widget.ticketId,
+        "clientId": widget.clientId,
+      }
+    };
+
+    String jsonMessage = json.encode(messageData);
+    print("Sending message: $jsonMessage");
+
+    try {
+      socket.emit('sendMessage', jsonMessage);
+      print('Message sent: $message');
+
+      final response = await http.post(
+        Uri.parse('https://chatdesk-prod.dialafrika.com/webchat/1/process'),
+        headers: {'Content-Type': 'application/json'},
+        body: jsonMessage,
+      );
+
+      if (response.statusCode == 200) {
+        print('Message sent successfully');
+      } else {
+        print('Failed to send message');
+      }
+    } catch (e) {
+      print('Error sending message: $e');
+    }
+
+    setState(() {
+      _messages.add(ChatMessage(
+        senderId: widget.clientId,
+        text: message,
+        clientId: widget.clientId,
+        timestamp: DateTime.now().toUtc(),
+      ));
+    });
+
+    _messageController.clear();
+  }
+
+
+  Future<void> retrieveMessageHistory() async {
+    final payload = {
+      "route": "RETRIEVE_CHAT",
+      "payload": {
+        "clientId": widget.clientId,
+      }
+    };
+
+    final response = await http.post(
+      Uri.parse('https://chatdesk-prod.dialafrika.com/webchat/1/process'),
+      headers: {'Content-Type': 'application/json'},
+      body: json.encode(payload),
+    );
+
+    if (response.statusCode == 200) {
+      final responseData = json.decode(response.body);
+      final threads = responseData['data']['payload']['tickets'][0]['threads'];
+      final List<ChatMessage> newMessages = [];
+
+      for (var thread in threads) {
+        final clientId = thread['clientId'];
+        final message = thread['message'];
+        final timestamp = DateTime.parse(thread['created_at']);
+
+        newMessages.add(ChatMessage(
+          senderId: clientId == widget.clientId ? clientId : widget.clientId,
+          text: message,
+          clientId: widget.clientId,
+          timestamp: timestamp,
         ));
+      }
+
+      setState(() {
+        _messages.addAll(newMessages); // Add the new messages to the existing list
       });
-      _messageController.clear();
+    } else {
+      print('Failed to retrieve message history');
     }
   }
 
+  void dispose() {
+    socket.dispose();
+    super.dispose();
+  }
+
+
   @override
   Widget build(BuildContext context) {
+    if (widget.clientId == null ||
+        widget.ticketId == null ||
+        widget.socketId == null) {
+      // Handle the case when values are null
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text('Error'),
+        ),
+        body: const Center(
+          child: Text('Error: Required data not available.'),
+        ),
+      );
+    }
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Chat'),
+        title: Text('ticketId: ${widget.ticketId}'),
       ),
       body: Column(
         children: [
           Expanded(
             child: ListView.builder(
               itemCount: _messages.length,
-              itemBuilder: (context, index) {
+              itemBuilder: (BuildContext context, int index) {
                 return _messages[index];
               },
             ),
           ),
+          const Divider(),
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: Row(
@@ -106,12 +195,18 @@ class _ChatScreenState extends State<ChatScreen> {
                 Expanded(
                   child: TextField(
                     controller: _messageController,
-                    decoration: const InputDecoration(hintText: 'Type a message...'),
+                    decoration: const InputDecoration(
+                      hintText: 'Type your message...',
+                    ),
                   ),
                 ),
                 IconButton(
                   icon: const Icon(Icons.send),
-                  onPressed: sendMessage,
+                  onPressed: () {
+                    if (_messageController.text.trim().isNotEmpty) {
+                      sendMessage(_messageController.text.trim());
+                    }
+                  },
                 ),
               ],
             ),
@@ -123,18 +218,26 @@ class _ChatScreenState extends State<ChatScreen> {
 }
 
 class ChatMessage extends StatelessWidget {
-  late String senderId;
+  final String senderId;
   final String clientId;
+  final DateTime timestamp;
   final String text;
 
-  ChatMessage({super.key, required this.senderId, required this.text, required this.clientId});
+  const ChatMessage({
+    super.key,
+    required this.senderId,
+    required this.text,
+    required this.clientId,
+    required this.timestamp,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
       child: Align(
-        alignment: senderId == clientId ? Alignment.centerRight : Alignment.centerLeft,
+        alignment:
+            senderId == clientId ? Alignment.centerRight : Alignment.centerLeft,
         child: Container(
           padding: const EdgeInsets.all(8.0),
           decoration: BoxDecoration(
--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,11 +1,94 @@
+import 'dart:convert';
+
 import 'package:bonga_chat_app/screens/features/inbox/chat_screen.dart';
 import 'package:flutter/material.dart';
-
+import 'package:socket_io_client/socket_io_client.dart' as IO;
 import '../features/faqs/categories_screen.dart';
 import '../features/services/services_screen.dart';
+import 'package:http/http.dart' as http;
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late IO.Socket socket;
+  bool chatInitialized = false;
+  String? clientId;
+  String? ticketId;
+  String? socketId;
+
+  @override
+  void initState() {
+    super.initState();
+    // Initialize Socket.IO connection
+    socket = IO.io('https://chatdesk-prod.dialafrika.com', <String, dynamic>{
+      'transports': ['websocket'],
+    });
+
+    socket.on('connect', (_) {
+      // Retrieve and store the socket ID
+      setState(() {
+        socketId = socket.id;
+      });
+      print('Socket.IO connected: ${socket.id}');
+    });
+  }
+
+  @override
+  void dispose() {
+    socket.dispose(); // Close the Socket.IO connection when done
+    super.dispose();
+  }
+
+  Future<void> initializeChat() async {
+    final response = await http.post(
+      Uri.parse('https://chatdesk-prod.dialafrika.com/webchat/1/process'),
+      headers: {'Content-Type': 'application/json'},
+      body: '{"route": "LIVE_CHAT"}',
+    );
+
+    if (response.statusCode == 200) {
+      // Parse the JSON response
+      final responseData = json.decode(response.body);
+      clientId = responseData['data']['payload']['message']
+          ['clientId']; // Assign to instance variable
+      ticketId = responseData['data']['payload']['message']
+          ['ticketId']; // Assign to instance variable
+
+      // Listen for Socket.IO events
+      socket.on('connect', (_) {
+        print('Socket.IO connected ${socket.id}}');
+        // socket.emit('join', {'clientId': clientId, 'ticketId': ticketId});
+      });
+      socket.connect(); // Connect to the Socket.IO server
+
+      socket.on('disconnect', (_) {
+        // Handle socket disconnection
+        print('Socket disconnected');
+      });
+
+      socket.on('message', (data) {
+        // Handle incoming messages
+        print('Received message: $data');
+      });
+      setState(() {
+        chatInitialized = true; // Update the UI state
+      });
+    } else {
+      print('Failed to initialize chat');
+    }
+
+    socket.on('message_from_agent', (data) {
+      // Handle incoming messages from the agent
+      print('Received message from agent: $data');
+      // You can update the UI or perform any other actions here
+    });
+
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,10 +103,31 @@ class HomePage extends StatelessWidget {
             child: buildSectionCard(
               title: 'Inbox',
               icon: Icons.mail_outline_rounded,
-              onTap: () {
+              onTap: () async {
+                // Initialize the chat
+                await initializeChat();
+                // Navigate to the ChatScreen
                 Navigator.push(
                   context,
-                  MaterialPageRoute(builder: (context) => const ChatScreen()),
+                  MaterialPageRoute(builder: (context) {
+                    if (clientId != null &&
+                        ticketId != null &&
+                        socketId != null) {
+                      return ChatScreen(
+                        clientId: clientId!,
+                        ticketId: ticketId!,
+                        socketId: socketId!,
+                        socket: socket,
+                      );
+                    } else {
+                      // Handle the case when clientId, ticketId, or socketId is null
+                      return Scaffold(
+                        body: Center(
+                          child: CircularProgressIndicator(),
+                        ),
+                      );
+                    }
+                  }),
                 );
               },
             ),
@@ -35,7 +139,8 @@ class HomePage extends StatelessWidget {
               onTap: () {
                 Navigator.push(
                   context,
-                  MaterialPageRoute(builder: (context) => const CategoryScreen()),
+                  MaterialPageRoute(
+                      builder: (context) => const CategoryScreen()),
                 );
               },
             ),
@@ -47,7 +152,8 @@ class HomePage extends StatelessWidget {
               onTap: () {
                 Navigator.push(
                   context,
-                  MaterialPageRoute(builder: (context) => const ServiceScreen()),
+                  MaterialPageRoute(
+                      builder: (context) => const ServiceScreen()),
                 );
               },
             ),
@@ -75,7 +181,8 @@ class HomePage extends StatelessWidget {
               const SizedBox(height: 16),
               Text(
                 title,
-                style: const TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
+                style:
+                    const TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
               ),
             ],
           ),
